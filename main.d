47384ced94e07e60129a0102c132b25d093f2351
Hinsx <92205623+Hinsx@users.noreply.github.com>
2023-02-17 13:17:34 +0800

    Delete main.d

--- main.d
+++
@@ -1,7 +0,0 @@
-main.o main.d : main.cpp Server/HttpServer.h Server/Lock.h Server/InetAddress.h \
- Server/../Log/Logger.h Server/../Log/LogStream.h \
- Server/../Log/../Server/Timestamp.h Server/HttpConnection.h \
- Server/Buffer.h Server/HttpContext.h Server/HttpRequest.h \
- Server/Threadpool.h Server/EventLoop.h Server/CurrentThread.h \
- Log/Logger.h Log/AsyncLogging.h Log/LogStream.h Log/../Server/Thread.h \
- Log/../Server/Lock.h Config.h Server/SQLpool.h
